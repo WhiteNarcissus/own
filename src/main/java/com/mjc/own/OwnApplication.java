11b976f80f08c921da4441ed37b448b8306f8cc7
Kevin <1119250897@qq.com>
2020-11-09 23:08:23 +0800

    项目启动要家 dev 要把 resource mark as Resource

--- src/main/java/com/mjc/own/OwnApplication.java
+++ src/main/java/com/mjc/own/OwnApplication.java
@@ -3,6 +3,7 @@ package com.mjc.own;
 import com.mjc.own.mapper.TkMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan(basePackages = {"com.mjc.own.mapper"}, markerInterface = TkMapper.class)
